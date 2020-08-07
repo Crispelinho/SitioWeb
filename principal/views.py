26715971cbb35f13e013d77f6781c3e1322e024d
Crispelinho <cristhian.carpio18@gmail.com>
2020-08-07 16:39:12 -0500

    Creación de vista de contactos con Bootstrap

--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from principal.models import Contacto
+from django.template import loader
+
 # Create your views here.
 
 def ListaContactos(request):
-    return HttpResponse("Hola mundo")
+    template = loader.get_template('contactos.html')
+    context = {
+        'contactos':Contacto.objects.all(),
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def index (request):
+
+    return render(request, 'index.html')
