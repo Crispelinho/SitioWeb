0179740f7042fc0cb785094ee55a4c59c758677c
Crispelinho <cristhian.carpio18@gmail.com>
2020-08-20 13:14:15 -0500

    Cambios en documents.py

--- DOCKER/sitioweb/gestioncontactos/documents.py
+++ DOCKER/sitioweb/gestioncontactos/documents.py
@@ -1,35 +1,28 @@
-from django_elasticsearch_dsl import Document
-from django_elasticsearch_dsl.registries import registry
-from .models import Car
+
+# -*- coding: utf-8 -*-
+from django_elasticsearch_dsl import DocType, Index, fields
+
+from .models import Contacto
+
+item = Index('item')
 
 
-@registry.register_document
-class CarDocument(Document):
-    class Index:
-        # Name of the Elasticsearch index
-        name = 'cars'
-        # See Elasticsearch Indices API reference for available settings
-        settings = {'number_of_shards': 1,
-                    'number_of_replicas': 0}
+@item.doc_type
+class ItemDocument(DocType):
+    """Document in elasticsearch with index item """
 
-    class Django:
-        model = Car # The model associated with this Document
+    price = fields.FloatField()
 
-        # The fields of the model you want to be indexed in Elasticsearch
+    class Meta:
+        model = Item
         fields = [
-            'name',
-            'color',
-            'description',
-            'type',
+            'id',
+            'nombre',
+            'apellido',
+            'Ncelular',
+            'fotogragia',
+            'correo'
         ]
 
-        # Ignore auto updating of Elasticsearch when a model is saved
-        # or deleted:
-        # ignore_signals = True
-
-        # Don't perform an index refresh after every update (overrides global setting):
-        # auto_refresh = False
-
-        # Paginate the django queryset used to populate the index with the specified size
-        # (by default it uses the database driver's default setting)
-        # queryset_pagination = 5000
+    def prepare_price(self, instance):
+        return float(instance.price)
